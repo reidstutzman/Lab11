524fc39c70b5401eebb1f984a9c47cdfa15215d6
reidstutzman <rstutzman3@gmail.com>
2021-04-26 19:49:33 -0500

    Merge two branches

--- fiboRecursive.m
+++ fiboRecursive.m
@@ -1,9 +1,9 @@
 function [fibonacciNumber] = fiboRecursive(n)
-gloabl count;
+global count;
    if n== 1
-    fibonacciNumber-1;
+    fibonacciNumber=1;
 elseif n==2
-    fibonacciNumber-1;
+    fibonacciNumber=1;
 else 
     count = count+1;
     fibonacciNumber = fiboRecursive(n-2) + fiboRecursive(n-1);
